2286f0cf5552778195db5176751801407498d3ca
Anagha Sethuraman <anagha.sethuraman@sjsu.edu>
2021-12-05 16:22:44 -0800

    Clinic-Vaccine-Disease-View

--- frontend/src/components/admin/Clinic.js
+++ frontend/src/components/admin/Clinic.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 import "./Admin.css";
-
+import {
+    Card
+   } from 'react-bootstrap';
+   
 class Clinic extends Component {
   render() {
     return (
       <div class="stageClinic">
       <h4 style={{color:'white', fontSize:'25px'}} >All Available Clinics</h4>
+      <div style={{ display: 'flex', justifyContent: '' }}>
+                 
+                 <Card style={{ width: "18rem" }}>
+                 <Card.Body>
+                 <Card.Title>
+                
+                   <h5>Name of the Clinic</h5>
+                  
+                   </Card.Title>
+                Business  Hrs:
+                <br/>
+                No of Physicians:
+                 </Card.Body> 
+                 </Card>                           
+      </div>
       </div>
     );
   }
--- frontend/src/components/admin/Disease.js
+++ frontend/src/components/admin/Disease.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 import "./Admin.css";
-
+import {
+    Card
+   } from 'react-bootstrap';
 class Disease extends Component {
   render() {
     return (
       <div class="stageClinic">
       <h4 style={{color:'white', fontSize:'25px'}} >Current Diseases</h4>
+      <div style={{ display: 'flex', justifyContent: '' }}>
+                 
+                 <Card style={{ width: "18rem" }}>
+                 <Card.Body>
+                 <Card.Title>
+                
+                   <h5>Name of the Disease</h5>
+                  
+                   </Card.Title>
+                    Description:
+                    
+                 </Card.Body> 
+                 </Card>                           
+      </div>
       </div>
     );
   }
